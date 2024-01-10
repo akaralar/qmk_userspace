dcc0c1e9274e4e43a1f5fb0051450514254cef33
Ahmet Karalar <akaralar@gmail.com>
2024-01-10 22:00:39 +0100

    Reorder rules

--- keyboards/ergodox_ez/keymaps/akaralar/rules.mk
+++ keyboards/ergodox_ez/keymaps/akaralar/rules.mk
@@ -1,23 +1,24 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LTO_ENABLE = yes
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-WEBUSB_ENABLE = no
-ORYX_ENABLE = no
+COMBO_ENABLE = no
 DYNAMIC_MACRO_ENABLE = yes
-PROGRAMMABLE_BUTTON_ENABLE = no
+DYNAMIC_TAPPING_TERM_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+RGB_MATRIX_ENABLE = yes
 SRC = matrix.c
 SRC += features/achordion.c
 SRC += features/casemodes.c
 SRC += features/custom_caps_lock.c
 SRC += features/custom_shift_keys.c
 SRC += features/debug_helper.c
-CAPS_WORD_ENABLE = yes
-RGB_MATRIX_ENABLE = yes
-DYNAMIC_TAPPING_TERM_ENABLE = yes
 UNICODE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+WEBUSB_ENABLE = no
+ORYX_ENABLE = no
+PROGRAMMABLE_BUTTON_ENABLE = no
 
 # Disable the following to save space
 SPACE_CADET_ENABLE = no
