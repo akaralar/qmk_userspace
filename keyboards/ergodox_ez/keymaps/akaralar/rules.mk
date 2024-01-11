bf50a81e53e2fb1b0890bacab3be2341c509b38c
Ahmet Karalar <akaralar@gmail.com>
2024-01-11 19:29:00 +0100

    Add Keymapp support for personal keymap

--- keyboards/ergodox_ez/keymaps/akaralar/rules.mk
+++ keyboards/ergodox_ez/keymaps/akaralar/rules.mk
@@ -1,24 +1,25 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LTO_ENABLE = yes
+CAPS_WORD_ENABLE = yes
 COMBO_ENABLE = no
+COMMAND_ENABLE = no
+CONSOLE_ENABLE = no
 DYNAMIC_MACRO_ENABLE = yes
 DYNAMIC_TAPPING_TERM_ENABLE = yes
-CAPS_WORD_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
+LTO_ENABLE = yes
+ORYX_ENABLE = yes
+PROGRAMMABLE_BUTTON_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+UNICODE_ENABLE = no
+WEBUSB_ENABLE = no
+
 SRC = matrix.c
 SRC += features/achordion.c
 SRC += features/casemodes.c
 SRC += features/custom_caps_lock.c
 SRC += features/custom_shift_keys.c
 SRC += features/debug_helper.c
-UNICODE_ENABLE = no
-KEY_OVERRIDE_ENABLE = no
-CONSOLE_ENABLE = no
-COMMAND_ENABLE = no
-WEBUSB_ENABLE = no
-ORYX_ENABLE = no
-PROGRAMMABLE_BUTTON_ENABLE = no
 
 # Disable the following to save space
 SPACE_CADET_ENABLE = no
