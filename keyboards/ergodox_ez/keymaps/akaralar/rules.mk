c7b6ab875ad1a61fd0d068a7655526634d9c23c7
Ahmet Karalar <akaralar@gmail.com>
2024-02-11 23:33:28 +0100

    Disable dynamic tapping term to enable combos
    Dynamic tapping term and combos don't play well in my keymap for some
    reason. This will be debugged later on.

--- keyboards/ergodox_ez/keymaps/akaralar/rules.mk
+++ keyboards/ergodox_ez/keymaps/akaralar/rules.mk
@@ -1,11 +1,11 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 CAPS_WORD_ENABLE = yes
-COMBO_ENABLE = no
+COMBO_ENABLE = yes
 COMMAND_ENABLE = no
 CONSOLE_ENABLE = no
 DYNAMIC_MACRO_ENABLE = yes
-DYNAMIC_TAPPING_TERM_ENABLE = yes
+DYNAMIC_TAPPING_TERM_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
 LTO_ENABLE = yes
 ORYX_ENABLE = yes
