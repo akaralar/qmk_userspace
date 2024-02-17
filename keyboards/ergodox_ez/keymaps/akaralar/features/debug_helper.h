e7a430fc636ff90ce4732014f3a9d3935ba1c95c
Ahmet Karalar <akaralar@gmail.com>
2024-02-17 16:18:06 +0100

    Add debugging helpers and prints to Achordion

--- keyboards/ergodox_ez/keymaps/akaralar/features/debug_helper.h
+++ keyboards/ergodox_ez/keymaps/akaralar/features/debug_helper.h
@@ -6,6 +6,8 @@
 #if CONSOLE_ENABLE
 void enable_debug_user(void);
 void prefixed_print(uint16_t keycode, keyrecord_t *record, const char* prefix);
+void prefixed_print_keycode(uint16_t keycode, const char* prefix);
+void prefixed_print_letter(uint16_t keycode, const char* prefix);
 void prefixed_print_layer(layer_state_t state, const char* prefix);
 void prefixed_print_all_mods(const char* prefix);
 #endif
